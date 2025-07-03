b4a544108b87b521e5ff0cd3768e27b490e0d2fb
habibur5313 <habiburmamun313@gmail.com>
2025-07-03 10:33:50 +0600

    cors origin added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import { booksRoutes } from './app/controllers/books.controllers';
 import { borrowsRoutes } from './app/controllers/borrow.conrtoller';
+import cors from "cors";
 const app: Application = express()
 
 app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://simple-library-management-nu.vercel.app'],
+}));
+
+// app.use(cors())
 
 app.use("/api/books", booksRoutes)
 app.use("/api/borrow", borrowsRoutes)
